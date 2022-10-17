e11fe98bd8e037d6c03c354e073ff9efdbb790ee
mons1201 <mohanmons1201@gmail.com>
2022-10-17 10:12:53 -0700

    back

--- sympossium/events/views.py
+++ sympossium/events/views.py
@@ -13,11 +13,24 @@ client=connect(host="mongodb+srv://mohan:mohan@cluster0.traigro.mongodb.net/?ret
 # Create your views here.
 
 def makeCreate(req):
-    eve=documents.Event()
+    if req.method=="POST":
+        #print("POST accepted")
+        nm=req.POST['evename']
+        dt=req.POST['evedate']
+        dp=req.POST['evedept']
+        #print(nm,dt,dp)
+        eve=documents.Event()
+        eve.initiate(nm,datetime.fromisoformat(dt),dp)
+        eve.save()
+        return render(req,'schedule.html',{"info":nm+ "has scheduled" })
+    else:
+        return render(req,'schedule.html')
+        
+    '''eve=documents.Event()
     eve.initiate("Sparkers17",datetime.fromisoformat("2022-10-10"),"EEE")
     eve.save()
     return HttpResponse('event has added')
-
+'''
 def makeList(req):
     mine=documents.Event.objects.all()
     for x in mine:
