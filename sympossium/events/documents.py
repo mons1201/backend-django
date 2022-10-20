f368ce014eef6aff57aaa3079f0a1a0ae759837a
mons1201 <mohanmons1201@gmail.com>
2022-10-20 10:44:46 -0700

    event

--- sympossium/events/documents.py
+++ sympossium/events/documents.py
@@ -7,13 +7,13 @@ class Event(Document):
     eveName=StringField()
     eveDate=DateField()
     eveDepartment=StringField()
-    eveParticipants=StringField(required=False)
+    eveParticipants=ListField(required=False)
     eveWinner=StringField(required=False)
 
     def _str__(self):
-        return "Event Information "+str(self.eveId)+" "+self.eveName+" "+str(self.eveDate)+" "+self.eveDepartment+" "+self.eveParticipants+" "+self.eveWinner+"\n"
+        return "Event Information "+str(self.eveId)+" "+self.eveName+" "+str(self.eveDate)+" "+self.eveDepartment+" "+str(self.eveParticipants)+" "+self.eveWinner+"\n"
         
-    def initiate(self,nm="",dt=datetime.utcnow(),dept="",part="",win=""):
+    def initiate(self,nm="",dt=datetime.utcnow(),dept="",part="[]",win=""):
         self.eveName=nm
         self.eveDate=dt
         self.eveDepartment=dept
